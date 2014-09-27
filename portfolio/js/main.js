72843959acaaba9cfbca73c17c12e6485d05869f
KevinDonkers <kevindonkers1@hotmail.com>
2014-09-27 12:09:54 -0400

    added comments and my portrait

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,4 +1,12 @@
-﻿$(document).foundation({
+﻿/*
+    main.js
+    Kevin Donkers
+    portfolio website
+    This is the main JavaScript file for Kevin Donkers portfolio website 
+*/
+
+//the settings for the magellan navigation bar
+$(document).foundation({
     "magellan-expedition": {
         active_class: 'active', // specify the class used for active sections
         threshold: 1, // how many pixels until the magellan bar sticks, 0 = auto
